26adabe774a4de25e7533b64cc2f3a915a50885b
ErilhirLD <jacob.marie.andrea.spam@gmail.com>
2026-04-19 23:37:39 +0200

    Cycle jour/nuit fonctionnel

---
+++ godot_project/day.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+func _process(delta: float) -> void:
+	$DayNumber.text = "Day %0*d" % [2, Global.day_number]
+
+func display_game_title(game_dialogue):
+	game_dialogue.visible = false
+	game_dialogue.clear()
+	
+	$GameTitle.visible = true
+	$AnimationPlayer.play("Game_title")
+	await $AnimationPlayer.animation_finished
+	
+	game_dialogue.visible = true
+	$GameTitle.visible = false
+
+func next_day():
+	Global.day_number += 1
