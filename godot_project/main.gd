7ab8cf3e818d39277973389aa2533cc1844d3422
ErilhirLD <jacob.marie.andrea.spam@gmail.com>
2026-04-20 16:38:51 +0200

    Correction de la main

--- godot_project/main.gd
+++ godot_project/main.gd
@@ -104,8 +104,6 @@ func end_of_day():
 	$Night.visible = true
 	night_timer.start_timer()
 	play_music(MUSIC_NIGHT)
-	remove_child($DialogueUi)
-	$Night/UI.add_child(game_dialogue)
 	print("current day: ", Global.day_number)
 	$Night.load_level(Global.day_number-1)
 	
